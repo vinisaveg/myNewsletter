41f3fd1e882aec7186a6f66909eb9c826364acd2
Saveg <savegdesigner@gmail.com>
2020-11-17 09:12:05 -0300

    starting development :rocket:

---
+++ src/components/layouts/Hero/Hero.component.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+import "./hero.css";
+
+interface HeroProps {}
+
+const Hero: FunctionComponent<HeroProps> = () => {
+  return <div className="hero"></div>;
+};
+
+export default Hero;
