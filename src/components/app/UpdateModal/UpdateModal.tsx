b2c7828d3c054fe51a58622fe82d371b9b94429e
savegnago <savegdesigner@gmail.com>
2021-02-17 09:14:54 -0300

    Added update modal form :zap:

--- src/components/app/UpdateModal/UpdateModal.tsx
+++ src/components/app/UpdateModal/UpdateModal.tsx
@@ -1,52 +1,104 @@
-import React, { FunctionComponent, useContext, useEffect } from "react";
+import { useFormik } from "formik";
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 import { AppContext } from "../../../context/context";
 import data from "../../../data/layouts";
+import { Wrapper, Input, ButtonWrapper } from "./styles";
+import { Button } from "../../../styles/shared/Button";
 
 const UpdateModal: FunctionComponent = () => {
   const [appContext, setAppContext] = useContext(AppContext);
+  const [initialValuesFormik, setInitialValuesFormik] = useState<any>({});
 
   useEffect(() => {
     console.log("Updating element has changed");
+
+    // Set the Formik Object Type to handle the inputs fields values to update
+    appContext.updatingLayoutFields.forEach((key) => {
+      setInitialValuesFormik({ [key]: "", ...initialValuesFormik });
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [appContext.updatingId]);
 
-  const handleUpdateElement = () => {
+  const formik = useFormik({
+    initialValues: initialValuesFormik,
+    enableReinitialize: true,
+    onSubmit: (formikValues) => handleUpdateElement(formikValues),
+  });
+
+  const handleUpdateElement = (formikValues: any) => {
     // Grab the new values from the inputs
+    console.log(formikValues);
 
-    // Find element Index to update
-    let elementIndexToUpdate = appContext.elements.findIndex(
-      (element) => element.id === "appContext.updatingId"
-    );
+    // // Find element Index to update
+    // let elementIndexToUpdate = appContext.elements.findIndex(
+    //   (element) => element.id === "appContext.updatingId"
+    // );
 
-    // Find the element name to update
-    let elementNameToUpdate = appContext.elements[elementIndexToUpdate].name;
+    // // Find the element name to update
+    // let elementNameToUpdate = appContext.elements[elementIndexToUpdate].name;
 
-    // Grab the correspondig Layout
-    let layoutToUpdate = data.find(
-      (layout) => layout.name === elementNameToUpdate
-    );
+    // // Grab the correspondig Layout
+    // let layoutToUpdate = data.find(
+    //   (layout) => layout.name === elementNameToUpdate
+    // );
 
-    // Return the new element with the fresh props from the inputs
-    let updatedElement = layoutToUpdate?.element({ props: "" });
+    // // Return the new element with the fresh props from the inputs
+    // let updatedElement = layoutToUpdate?.element({ props: "" });
 
-    // Create a new Elements Array to set the updated element
-    let updatedElementsArray = appContext.elements;
+    // // Create a new Elements Array to set the updated element
+    // let updatedElementsArray = appContext.elements;
 
-    // Set the Updated Component into the new Elements array
-    if (updatedElement) {
-      updatedElementsArray[elementIndexToUpdate].component = updatedElement;
+    // // Set the Updated Component into the new Elements array
+    // if (updatedElement) {
+    //   updatedElementsArray[elementIndexToUpdate].component = updatedElement;
 
-      // Update the Context with the new Elements Array
-      setAppContext({ ...appContext, elements: updatedElementsArray });
-    }
+    //   // Update the Context with the new Elements Array
+    //   setAppContext({ ...appContext, elements: updatedElementsArray });
+    // }
   };
 
   // Show the inputs to be updated
   if (appContext.isUpdating) {
     return (
-      <div>
-        <span>Update Element</span>
-      </div>
+      <Wrapper onSubmit={formik.handleSubmit}>
+        {appContext.updatingLayoutFields.map((field) => (
+          <Input
+            onChange={formik.handleChange}
+            key={field}
+            id={field}
+            name={field}
+            type="text"
+            placeholder={field}
+          />
+        ))}
+
+        <ButtonWrapper>
+          <Button
+            type="submit"
+            margin={["10px", "10px", "0px", "0px"]}
+            color="white"
+            bgColor="#acacac;"
+          >
+            Cancel
+          </Button>
+
+          <Button
+            type="submit"
+            margin={["10px", "0px", "0px", "10px"]}
+            color="white"
+            bgColor="#f5476a;"
+          >
+            Update
+          </Button>
+        </ButtonWrapper>
+      </Wrapper>
     );
   }
 
